3059623fc9e86d5fec92558e45ab39b2580230fd
Allen Vargas <allen_carlo_vargas@yahoo.com>
2018-05-10 17:22:45 +1200

    updated final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -18,7 +19,7 @@ app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
-let token = "EAACrDNc3L0IBAJT9tUbsse0fkCuk0F3grQTjF8Y72KRoC5im8qe7lBbe8J8s9DmGZB16eBDxDfTeOdRGv3eTs5RZCpYBtrEyrZC4inooPMZBf7OT89NUt6qi1XrO6jNfq2PXNgXFsAZBZBskq6SBCSHo2fh6xU80shpUdGSZBbRIKhZCBdFpA9Eb"
+let token = "EAACrDNc3L0IBAHkcTKZC8xbZBDenZCo0217g30PZCGPoSaK8SzsmQRg5wei4kdMIC0KC6C7ZBI4n3MYHmX99vkIQdkNOP8g8ZA3uBTd42p5Ch88rhEPEHu7cn1bFhzZCyXTE8aTHmYPqEqHjazzDg31eI0ZCxIPiQ7oVYk6oTg1K4UGOLU4v3OAZB"
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
@@ -28,34 +29,42 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
-app.post('/webhook/', function(req, res){
+app.post('/webhook/', function(req, res)
+{
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++)
 	{
-		let event = messaging_events[i]
+		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
-		if(event.message && event.message.text){
+		if(event.message && event.message.text)
+		{
 			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0,100))
+			sendTextMessage(sender, "Text echo: " + text.substring(0,200))
 		}
 	}
 	res.sendStatus(200)
 })
 
-function sendText(sender, text){
+function sendText(sender, text)
+{
 	let messageData = {text: text}
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
 		qs : {acess_token : token},
 		method: "POST",
-		json: {
+		json:{
 			recipient: {id: sender},
 			message: messageData,
 		}
-	}, function(error, response, body){
-		if(error){
+	}, 
+	function(error, response, body)
+	{
+		if(error)
+		{
 			console.log("sending error")
-		}else if(response.body.error){
+		}
+		else if(response.body.error)
+		{
 			console.log("response body error")
 		}
 	})
